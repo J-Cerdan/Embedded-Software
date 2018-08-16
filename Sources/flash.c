905746ec174f23ce2337112f83e0c70c0b76dfd3
12544312 <12544312@LAB1111403010.adsroot.uts.edu.au>
2018-08-16 13:09:52 +1000

    began flash.c

---
+++ Sources/flash.c
@@ -0,0 +1,113 @@
+/*! @file
+ *
+ *  @brief Routines for erasing and writing to the Flash.
+ *
+ *  This contains the functions needed for accessing the internal Flash.
+ *
+ *  @author Amir and Joseph
+ *  @date 2018-08-16
+ */
+
+// new types
+#include "flash.h"
+
+struct
+{
+  uint8_t command;
+
+  struct
+  {
+    uint8_t address1 = 0x08;
+    uint8_t address2 = 0x00;
+    uint8_t address3 = 0x00;
+  } address;
+
+  struct
+  {
+    uint8_t byte0;
+    uint8_t byte1;
+    uint8_t byte2;
+    uint8_t byte3;
+    uint8_t byte4;
+    uint8_t byte5;
+    uint8_t byte6;
+    uint8_t byte7;
+  } data;
+
+} FCCOB;
+
+FCCOB fccob;
+
+/*! @brief Enables the Flash module.
+ *
+ *  @return bool - TRUE if the Flash was setup successfully.
+ */
+bool Flash_Init(void)
+{
+  return 0;
+}
+
+/*! @brief Allocates space for a non-volatile variable in the Flash memory.
+ *
+ *  @param variable is the address of a pointer to a variable that is to be allocated space in Flash memory.
+ *         The pointer will be allocated to a relevant address:
+ *         If the variable is a byte, then any address.
+ *         If the variable is a half-word, then an even address.
+ *         If the variable is a word, then an address divisible by 4.
+ *         This allows the resulting variable to be used with the relevant Flash_Write function which assumes a certain memory address.
+ *         e.g. a 16-bit variable will be on an even address
+ *  @param size The size, in bytes, of the variable that is to be allocated space in the Flash memory. Valid values are 1, 2 and 4.
+ *  @return bool - TRUE if the variable was allocated space in the Flash memory.
+ *  @note Assumes Flash has been initialized.
+ */
+bool Flash_AllocateVar(volatile void** variable, const uint8_t size)
+{
+
+}
+
+/*! @brief Writes a 32-bit number to Flash.
+ *
+ *  @param address The address of the data.
+ *  @param data The 32-bit data to write.
+ *  @return bool - TRUE if Flash was written successfully, FALSE if address is not aligned to a 4-byte boundary or if there is a programming error.
+ *  @note Assumes Flash has been initialized.
+ */
+bool Flash_Write32(volatile uint32_t* const address, const uint32_t data)
+{
+
+}
+
+/*! @brief Writes a 16-bit number to Flash.
+ *
+ *  @param address The address of the data.
+ *  @param data The 16-bit data to write.
+ *  @return bool - TRUE if Flash was written successfully, FALSE if address is not aligned to a 2-byte boundary or if there is a programming error.
+ *  @note Assumes Flash has been initialized.
+ */
+bool Flash_Write16(volatile uint16_t* const address, const uint16_t data)
+{
+  Flash_Write32();
+}
+
+/*! @brief Writes an 8-bit number to Flash.
+ *
+ *  @param address The address of the data.
+ *  @param data The 8-bit data to write.
+ *  @return bool - TRUE if Flash was written successfully, FALSE if there is a programming error.
+ *  @note Assumes Flash has been initialized.
+ */
+bool Flash_Write8(volatile uint8_t* const address, const uint8_t data)
+{
+  Flash_Write16();
+}
+
+/*! @brief Erases the entire Flash sector.
+ *
+ *  @return bool - TRUE if the Flash "data" sector was erased successfully.
+ *  @note Assumes Flash has been initialized.
+ */
+bool Flash_Erase(void)
+{
+   &= 0xFFFF;
+}
+
